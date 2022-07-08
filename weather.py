ef2b87e0958a474acc3dd4ee559bade5e6541fd2
Rhim Andemichael <rhima@princeton.edu>
2022-07-08 07:23:41 +0000

    added new endpoints
    Co-authored-by: Mark Karomo <mk772@cairn.edu>

--- weather.py
+++ weather.py
@@ -6,45 +6,72 @@ import pandas as pd
 import sqlalchemy as db
 from sqlalchemy import create_engine 
 
-city_input = input("Enter your city name: ")
-days_input = input("Enter number of days for your forecast: ")
 
-url = 'http://api.weatherapi.com/v1/forecast.json'
+option_input = input("Enter one weather option: Forecast, Realtime Weather, History: ")
 
-querystring = {"q":{city_input},"days":{days_input}}
 
-headers = {
+if option_input == "Forecast":
+    city_input = input("Enter your city name: ")
+    days_input = input("Enter number of days for your forecast: ")
+
+    url = 'http://api.weatherapi.com/v1/forecast.json'
+    querystring = {"q":{city_input},"days":{days_input}}
+    headers = {
 	"key": "fdab0fe0b1d1492c889224703220707"
-}
+    }
+    response = requests.get(url, headers=headers, params=querystring)
 
-response = requests.get(url, headers=headers, params=querystring)
+    weather_data = response.json()['forecast']
 
-weather_data = response.json()['forecast']
+    empty_list1 = []
+    empty_list2 = []
+    empty_list3 = []
+    empty_list4 = []
+    empty_list5 = []
+    data_store = weather_data['forecastday']
+    for data_access in data_store:
+        date = data_access['date']
+        max_temp = data_access['day']['maxtemp_f']
+        min_temp = data_access['day']['mintemp_f']
+        avg_hum = data_access['day']['avghumidity']
+        avg_temp = data_access['day']['avgtemp_f']
+        empty_list1.append(date)
+        empty_list2.append(max_temp)
+        empty_list3.append(min_temp)
+        empty_list4.append(avg_temp)
+        empty_list5.append(avg_hum)
 
-#print(weather_data['forecast'])
-#pprint.pprint(weather_data['forecast'])
-
-empty_list1 = []
-empty_list2 = []
-empty_list3 = []
-empty_list4 = []
-empty_list5 = []
-data_store = weather_data['forecastday']
-for data_access in data_store:
-  date = data_access['date']
-  max_temp = data_access['day']['maxtemp_f']
-  min_temp = data_access['day']['mintemp_f']
-  avg_hum = data_access['day']['avghumidity']
-  avg_temp = data_access['day']['avgtemp_f']
-  empty_list1.append(date)
-  empty_list2.append(max_temp)
-  empty_list3.append(min_temp)
-  empty_list4.append(avg_temp)
-  empty_list5.append(avg_hum)
-
-subs_dataframe = pd.DataFrame(list(zip(empty_list1, empty_list2, empty_list3, empty_list4, empty_list5)),
+    weather_dataframe = pd.DataFrame(list(zip(empty_list1, empty_list2, empty_list3, empty_list4, empty_list5)),
                columns =['Date', 'MaxTemp', 'MinTemp', 'AvgTemp', 'AvgHumidity'])
 
-print(subs_dataframe)
+
+    engine = db.create_engine('sqlite:///forecast_weather.db')
+    weather_dataframe.to_sql('weather_info', con=engine, if_exists='replace', index=False)
+    query_result = engine.execute("SELECT * FROM weather_info;").fetchall()
+    df = pd.DataFrame(query_result)
+
+    df.columns = ["Date", "MaxTemp", "MinTemp", "AvgTemp", "AvgHumidity"]
+
+    print(df)
+
+if option_input == "Realtime Weather":
+    city_input = input("Enter your city name: ")
+    url = "https://weatherapi-com.p.rapidapi.com/current.json"
+    querystring = {"q":{city_input}}
+    headers = {
+	    "key": "fdab0fe0b1d1492c889224703220707"
+    }
+    response = requests.get(url, headers=headers, params=querystring)
+    print(reponse.json())
 
 
+if option_input == "History":
+    city_input = input("Enter your city name: ")
+    date_input = input("Enter date in the format yyyy-mm-dd: ")
+    url = "https://weatherapi-com.p.rapidapi.com/history.json"
+    querystring = {"q":{city_input},"dt":{date_input},"lang":"en"}
+    headers = {
+	    "key": "fdab0fe0b1d1492c889224703220707"
+    }
+    response = requests.get(url, headers=headers, params=querystring)
+    print(response.json())
